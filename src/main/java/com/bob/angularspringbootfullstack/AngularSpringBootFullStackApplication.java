fd1cb20587dc3c4c9575f2d23c31c6c59c912d3f
Bobby Oliver <robertcoliver@lewisu.edu>
2026-04-07 23:42:59 -0700

    Initialize Spring Boot app

---
+++ src/main/java/com/bob/angularspringbootfullstack/AngularSpringBootFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.bob.angularspringbootfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBootFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AngularSpringBootFullStackApplication.class, args);
+    }
+
+}
