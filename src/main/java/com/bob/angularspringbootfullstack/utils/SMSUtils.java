765591f4a3b2879f1f77057bda2fe037092770f8
Bobby Oliver <robertcoliver@lewisu.edu>
2026-04-18 23:17:34 -0700

    Adding setup for Twilio - make sure to comment out sendSMS() method in UserRepoImpl.java to avoid charges !

--- src/main/java/com/bob/angularspringbootfullstack/utils/SMSUtils.java
+++ src/main/java/com/bob/angularspringbootfullstack/utils/SMSUtils.java
@@ -10,11 +10,11 @@ public class SMSUtils {
     // TODO READ BELOW!
     //these have been removed for security purposes. Feel free to re-add later during showcase of the application
     public static final String FROM_NUMBER = "";
-    public static final String SID_KEY = "";
-    public static final String TOKEN_KEY = "";
+    public static final String FAKE_ONE = "";
+    public static final String FAKE_TWO = "";
 
     public static void sendSMS(String toNumber, String messageBody) {
-        Twilio.init(SID_KEY, TOKEN_KEY);
+        Twilio.init(FAKE_ONE, FAKE_TWO);
         Message message = Message.creator(
                 new PhoneNumber("+1" + toNumber),
                 new PhoneNumber(FROM_NUMBER),
